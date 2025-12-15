fb4fd395a7585c35d3962955c3738be9e7655f59
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2025-12-14 17:55:08 -0800

    Update Starfield.js

--- Starfield.js
+++ Starfield.js
@@ -266,7 +266,7 @@ function moveStars() {
     STAR.momentumY *= 0.97;
 
     // Screen wrap if passive
-    if(USER_SPEED > 0) {
+    if(USER_SPEED = 0 || FADE_WITH_DISTANCE < 0.005) {
       STAR.x = (STAR.x % WIDTH + WIDTH) % WIDTH;
       STAR.y = (STAR.y % HEIGHT + HEIGHT) % HEIGHT;
     }
