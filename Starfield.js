270211b0f128cf1991f9339c15fad6f22136ab40
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2025-12-16 07:24:28 -0800

    Update Starfield.js

--- Starfield.js
+++ Starfield.js
@@ -264,14 +264,14 @@ function moveStars() {
     
 // User gravity ring (attract from outside)
 STAR.momentumX +=
-  2.5e5 *
+  3e5 *
   USER_SPEED *
   X_DISTANCE *
   (INV_SCREEN_SIZE ** 5) *
   (FADE_WITH_DISTANCE ** (INV_SCREEN_SIZE * 4.5));
 
 STAR.momentumY +=
-  2.5e5 *
+  3e5 *
   USER_SPEED *
   Y_DISTANCE *
   (INV_SCREEN_SIZE ** 5) *
