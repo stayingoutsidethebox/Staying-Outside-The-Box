fe99be9a0e27c5d364d07b431dc7041ac792d5b1
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2025-12-16 06:30:35 -0800

    Update Starfield.js

--- Starfield.js
+++ Starfield.js
@@ -280,14 +280,14 @@ STAR.momentumY +=
 
 // User gravity ring (repel from inside)
 STAR.momentumX -=
-  4.0e7 *
+  6.0e7 *
   USER_SPEED *
   X_DISTANCE *
   (INV_SCREEN_SIZE ** 5) *
   (FADE_WITH_DISTANCE ** (INV_SCREEN_SIZE * 6));
 
 STAR.momentumY -=
-  4.0e7 *
+  6.0e7 *
   USER_SPEED *
   Y_DISTANCE *
   (INV_SCREEN_SIZE ** 5) *
