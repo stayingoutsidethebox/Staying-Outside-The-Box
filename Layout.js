be98bb4dcf351d76ba77c4cb5a5be9bf0f4af81b
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2025-12-16 12:29:40 -0800

    Update Layout.js

--- Layout.js
+++ Layout.js
@@ -83,7 +83,7 @@ window.addEventListener("load", () => {
   // Trigger slide-in
   requestAnimationFrame(() => {
     PAGE.classList.add("ready");
-    PAGE.addEventListener("transitionend", () => lockScrollToContainer(PAGE), { once: true });
+    //PAGE.addEventListener("transitionend", () => lockScrollToContainer(PAGE), { once: true });
   });
 
   // Back button visibility:
@@ -114,7 +114,7 @@ window.addEventListener("pageshow", (event) => {
   if (event.persisted || performance?.getEntriesByType("navigation")[0]?.type === "back_forward") {
     PAGE.classList.remove("slide-out");
     PAGE.classList.add("ready");
-    lockScrollToContainer(PAGE);
+    //lockScrollToContainer(PAGE);
     IS_TRANSITIONING = false;
     PAGE.scrollTop = 0;
   }
