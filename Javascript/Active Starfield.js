90a0115b548433230615d477ce9092a2a2e2703e
Sabian Colbert <31480512+sabiancolbert@users.noreply.github.com>
2025-12-27 00:21:36 -0600

    Update Active Starfield.js

--- Javascript/Active Starfield.js
+++ Javascript/Active Starfield.js
@@ -163,7 +163,7 @@ S.updateStarPhysics = function updateStarPhysics() {
       );
 
       const ATTRACTION_FORCE =
-        ((SETTINGS.attractStrength * 0.001) * SCALE.attractionForce) *
+        ((SETTINGS.attractStrength * 0.003) * SCALE.attractionForce) *
         S.pointerSpeedUnits *
         ATTRACTION_SHAPE;
 
