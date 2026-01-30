8a960cee85fa4551bd40b0d75ea448130eb4e212
Staying Outside The Box <31480512+stayingoutsidethebox@users.noreply.github.com>
2026-01-30 12:51:28 -0600

    Delete Javascript/Home.js

--- Javascript/Home.js
+++
@@ -1,9 +0,0 @@
-// Runs ASAP (no defer) so the correct CSS state applies before first paint.
-
-(function () {
-  const html = document.documentElement;
-
-  // Homescreen CSS if homescreen
-  html.classList.remove("otherJs");
-  html.classList.add("homeJs");
-})();
