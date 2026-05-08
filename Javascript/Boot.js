24d83286d7e65ca106eebd84620e901c8b96f75c
Staying Outside The Box <31480512+stayingoutsidethebox@users.noreply.github.com>
2026-05-08 17:38:09 -0500

    Update Boot.js

--- Javascript/Boot.js
+++ Javascript/Boot.js
@@ -4,7 +4,7 @@
   /* ===============================
    * 0) SITE VERSION (bump per deploy)
    * =============================== */
-  const SITE_VERSION = "05.08.2026.E";
+  const SITE_VERSION = "05.08.2026.F";
   window.SITE_VERSION = SITE_VERSION;
 
   /* ===============================
